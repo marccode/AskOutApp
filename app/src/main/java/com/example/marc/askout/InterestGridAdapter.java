b208791dd246a3e2e040d272c588ee74e732d707
Marc <marcmpescola@gmail.com>
2015-05-14 13:29:49 +0200

    Interessos ja casi està... Queda trobar icones xules i posar la url de la API definitiva

--- app/src/main/java/com/example/marc/askout/InterestGridAdapter.java
+++ app/src/main/java/com/example/marc/askout/InterestGridAdapter.java
@@ -2,6 +2,7 @@ package com.example.marc.askout;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,15 +11,29 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 /**
  * Created by marc on 5/12/15.
  */
 public class InterestGridAdapter extends BaseAdapter {
 
-    String[] result = {"Espectacles", "Música", "Cinema", "Museu", "Infantil", "Esport", "Exposició", "Art", "Ciència", "Oci i Cultura"};
     Context context;
-    public int[] imageId = {R.drawable.icon, R.drawable.icon, R.drawable.icon, R.drawable.icon, R.drawable.icon, R.drawable.icon, R.drawable.icon, R.drawable.icon, R.drawable.icon, R.drawable.icon};
-    public static int[] imageSelId = {R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action};
+
+    String[] result = {"Espectacles", "Música", "Cinema", "Museu", "Infantil", "Esport", "Exposició", "Art", "Ciència", "Oci i Cultura"};
+    public int[] imageId = {R.drawable.icon, R.drawable.ic_musica_no_sel, R.drawable.ic_cinema_no_sel, R.drawable.icon, R.drawable.icon, R.drawable.icon, R.drawable.icon, R.drawable.icon, R.drawable.icon, R.drawable.icon};
+    public int[] imageSelId = {R.drawable.ic_action, R.drawable.ic_musica_sel, R.drawable.ic_cinema_sel, R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action, R.drawable.ic_action};
+    public boolean[] selected = {false, false, false, false, false, false, false, false, false, false};
+
     private static LayoutInflater inflater = null;
 
     //public InterestGridAdapter(MainActivity mainActivity, String[] prgmNameList, int[] prgmImages) {
@@ -60,27 +75,6 @@ public class InterestGridAdapter extends BaseAdapter {
     public View getView(final int position, View convertView, ViewGroup parent) {
         // TODO Auto-generated method stub
 
-            /*
-            Holder holder;
-            if (convertView == null) {
-
-                convertView = inflater.inflate(R.layout.fragment_interests, parent, false);
-                holder = new Holder();
-                holder.img = (ImageView) convertView.findViewById(R.id.imageView1);
-                holder.tv = (TextView) convertView.findViewById(R.id.textView1);
-                convertView.setTag(holder);
-            } else {
-                holder = (Holder) convertView.getTag();
-            }
-
-            ImageView imageView = holder.img;
-            TextView textView = holder.tv;
-            imageView.setImageResource(imageId[position]);
-            textView.setText(result[position]);
-
-            return convertView;
-            */
-
         Holder holder = new Holder();
         final View rowView;
 
@@ -101,14 +95,57 @@ public class InterestGridAdapter extends BaseAdapter {
 
                 // DIRLI A LA API QUE ARA M'INTERESSA TAL
 
-                // CANVIAR LA ICONA
 
-                //ImageView imageView = (v.getTag()).img;
-                ImageView img = (ImageView) v;
-                //img.setImageResource(imageSelId[position]);
-                img.setImageResource(R.drawable.ic_action);
+                // CANVIAR LA ICONA
+                ImageView img = (ImageView) v.findViewById(R.id.imageView1);
+
+                if (selected[position]) {
+                    new RequestTask().execute("http://jediantic.upc.es/api/users/55536e7e0e1bbbb5b3b85bec/" + result[position] + "/false");
+                    selected[position] = false;
+                    img.setImageResource(imageId[position]);
+                }
+                else {
+                    new RequestTask().execute("http://jediantic.upc.es/api/users/55536e7e0e1bbbb5b3b85bec/" + result[position] + "/true");
+                    selected[position] = true;
+                    img.setImageResource(imageSelId[position]);
+                }
             }
         });
         return rowView;
     }
+
+    class RequestTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try {
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    responseString = out.toString();
+                    out.close();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+                //TODO Handle problems..
+            } catch (IOException e) {
+                //TODO Handle problems..
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+        }
+    }
+
 }
